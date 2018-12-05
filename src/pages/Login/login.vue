2df5ace008aaba899e041e6833323eb92fd7c7bb
heng <luoheng@yihuisoft.com>
2018-12-05 16:13:42 +0800

    'login登录样式修改'

--- src/pages/Login/login.vue
+++ src/pages/Login/login.vue
@@ -89,3 +89,30 @@
         margin-top: 6%;
     }
 </style>
+
+<style>
+
+    /**
+     * 修改登录按钮样式
+     */
+    .logdiv .el-button {
+        display: inline-block;
+        line-height: 1;
+        white-space: nowrap;
+        cursor: pointer;
+        background: #409EFF;
+        border: 1px solid #409EFF;
+        color: #fff;
+        -webkit-appearance: none;
+        text-align: center;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        outline: 0;
+        -webkit-transition: .1s;
+        transition: .1s;
+        font-weight: 500;
+        padding: 12px 20px;
+        font-size: 14px;
+        border-radius: 4px;
+    }
+</style>
