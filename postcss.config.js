8671a61de1c19bd46b52b46ed01535d983776e12
heng <luoheng@yihuisoft.com>
2018-12-17 20:14:21 +0800

    增加自适应差距

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    /*自适应*/
+    autoprefixer: {},
+      "postcss-pxtorem": {
+          "rootValue": 75, // 设计稿宽度的1/10,（JSON文件中不加注释，此行注释及下行注释均删除）
+          "propList":["*"] // 需要做转化处理的属性，如`hight`、`width`、`margin`等，`*`表示全部
+      }
+
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -82,10 +82,21 @@ module.exports = {
                 // @/ 是 src/ 的别名
                 // 所以这里假设你有 `src/assets/css/varuables.scss` 这个文件
                 data: `@import "@/assets/css/varuables.scss";`
+            },
+            /*自适应*/
+            postcss: {
+                // 这是rem适配的配置  注意： remUnit在这里要根据lib-flexible的规则来配制，如果您的设计稿是750px的，用75就刚刚好。
+                plugins: [
+                    require("postcss-px2rem")({
+                        remUnit: 75
+                    })
+                ]
             }
-        }
+        },
+
     },
-    // 三方插件的选项
+
+    // 第三方插件的选项
     pluginOptions: {
         // ...
     }
