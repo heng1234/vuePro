9c7efaa63dd7e3b6e0a0cb966e492236031b8da7
heng <luoheng@yihuisoft.com>
2019-01-09 21:10:22 +0800

    端口号

--- vue.config.js
+++ vue.config.js
@@ -66,7 +66,7 @@ module.exports = {
     devServer: {
         open: process.platform === 'darwin',
         host: '0.0.0.0',
-        port: 8081,
+        port: 9999,
         https: false,
         hotOnly: false,
         // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/cli-service.md#配置代理
