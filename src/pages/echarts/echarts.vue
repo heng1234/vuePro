4e1a68b3a2379452fb5baee2d162b60c83eb0854
heng <luoheng@yihuisoft.com>
2019-01-16 16:55:48 +0800

    增加echart图

--- src/pages/echarts/echarts.vue
+++ src/pages/echarts/echarts.vue
@@ -5,6 +5,8 @@
         </div>
         <div id="myChart" class="lft clearfix" :style="{width: '600px', height: '300px'}"></div>
         <div id="myMap"  class="lft clearfix" ref="myMap" style="width: 600px; height: 300px;"></div>
+        <div id="mybt"  class="lft clearfix" ref="mybt" style="width: 600px; height: 300px;"></div>
+        <div id="mybts"  class="lft clearfix" ref="mybts" style="width: 600px; height: 300px;"></div>
     </div>
 </template>
 
@@ -34,6 +36,8 @@ import option from './js/map-option'
         mounted(){
         this.creatE();
         this.drawChinaMap();
+        this.creatbt();
+        this.creatbts();
 
     },
        updated() {
@@ -77,6 +81,159 @@ import option from './js/map-option'
                     data: [5, 20, 36, 10, 10, 20]
                 }]
             });
+        },
+        creatbt(){
+
+            let myChart = this.$echarts.init(document.getElementById('mybt'))
+            // 绘制图表
+            myChart.setOption({
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b}: {c} ({d}%)",
+                },
+                legend: {
+                    orient: 'vertical',
+                    x: 'left',
+                    data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+                },
+                series: [
+                    {
+                        name:'访问来源',
+                        type:'pie',
+                        radius: ['50%', '70%'],
+                        avoidLabelOverlap: false,
+                        label: {
+                            normal: {
+                                show: false,
+                                position: 'center'
+                            },
+                            emphasis: {
+                                show: true,
+                                textStyle: {
+                                    fontSize: '16',
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        },
+                        labelLine: {
+                            normal: {
+                                show: false
+                            }
+                        },
+                        data:[
+                            {value:335, name:'直接访问'},
+                            {value:310, name:'邮件营销'},
+                            {value:234, name:'联盟广告'},
+                            {value:135, name:'视频广告'},
+                            {value:1548, name:'搜索引擎'}
+                        ]
+                    }
+                ]
+            }
+
+        );
+        },
+        creatbts(){
+
+            let myChart = this.$echarts.init(document.getElementById('mybts'))
+            // 绘制图表
+            myChart.setOption(
+                {
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b}: {c} ({d}%)"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        x: 'left',
+                        data:['直达','营销广告','搜索引擎','邮件营销','联盟广告','视频广告','百度','谷歌','必应','其他']
+                    },
+                    series: [
+                        {
+                            name:'访问来源',
+                            type:'pie',
+                            selectedMode: 'single',
+                            radius: [0, '30%'],
+
+                            label: {
+                                normal: {
+                                    position: 'inner'
+                                }
+                            },
+                            labelLine: {
+                                normal: {
+                                    show: false
+                                }
+                            },
+                            data:[
+                                {value:335, name:'直达', selected:true},
+                                {value:679, name:'营销广告'},
+                                {value:1548, name:'搜索引擎'}
+                            ]
+                        },
+                        {
+                            name:'访问来源',
+                            type:'pie',
+                            radius: ['40%', '55%'],
+                            label: {
+                                normal: {
+                                    formatter: ' {b|{b}：}{c}  {per|{d}%}  ',/*{a|{a}}{abg|}{hr|}*/
+                                    backgroundColor: '#fff',
+                                    borderColor: '#409EFF',
+                                    borderWidth: 1,
+                                    borderRadius: 4,
+                                    // shadowBlur:3,
+                                    // shadowOffsetX: 2,
+                                    // shadowOffsetY: 2,
+                                    // shadowColor: '#999',
+                                    // padding: [0, 7],
+                                    rich: {
+                                        a: {
+                                            color: '#999',
+                                            lineHeight: 22,
+                                            align: 'center'
+                                        },
+                                        // abg: {
+                                        //     backgroundColor: '#333',
+                                        //     width: '100%',
+                                        //     align: 'right',
+                                        //     height: 22,
+                                        //     borderRadius: [4, 4, 0, 0]
+                                        // },
+                                        hr: {
+                                            borderColor: '#aaa',
+                                            width: '100%',
+                                            borderWidth: 0.5,
+                                            height: 0
+                                        },
+                                        b: {
+                                            fontSize: 16,
+                                            lineHeight: 33
+                                        },
+                                        per: {
+                                            color: '#eee',
+                                            backgroundColor: '#334455',
+                                            padding: [2, 4],
+                                            borderRadius: 2
+                                        }
+                                    }
+                                }
+                            },
+                            data:[
+                                {value:335, name:'直达'},
+                                {value:310, name:'邮件营销'},
+                                {value:234, name:'联盟广告'},
+                                {value:135, name:'视频广告'},
+                                {value:1048, name:'百度'},
+                                {value:251, name:'谷歌'},
+                                {value:147, name:'必应'},
+                                {value:102, name:'其他'}
+                            ]
+                        }
+                    ]
+                }
+
+        );
         },
 
     }
--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -1,17 +1,54 @@
-<template>
-    <div>
-        <i class="iconfont hlvy-icon-jiaowu"></i>
-        主页
+ <template>
+    <div style="height: 100%;width: 100%;">
+        <div>
+            <hlvy-title :text="'系统首页'" :icon="'iconfont hlvy-icon-xueyuanguanliicon-'"></hlvy-title>
+        </div>
+
+        <div  style="height: 97%;width: 100%;background-color: rgba(238,245,253,0.9);">
+            <div class="homediv">
+                <span class="spanmoney"><span>总销售额:</span><p>&yen;5201314</p></span>
+                <div class="hnextdiv">
+                    hlvy
+                </div>
+            </div>
+
+        </div>
 
     </div>
 </template>
 
 <script>
     export default {
-        name: "homePage"
+        name: "homePage",
+        data(){
+            return{
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+.homediv{
+    float: left;
+    border: 1px solid #ffffff;
+    background-color: #ffffff;
+    width: 460px;
+    height: 200px;
+    box-shadow:2px 2px 2px #aaaaaa;
+    margin: 40px auto auto 40px;
+}
+.spanmoney>span{
+    display: inline-block;
+    color: rgb(194,196,199);
+    font-size: 22px;
+    margin: 7px auto auto 10px;
+}
+.spanmoney>p{
+font-size: 32px;
+text-indent: 12px;
+}
+.hnextdiv{
+    text-indent: 12px;
+    margin-top: 10px;
+}
 </style>
